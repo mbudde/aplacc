0b8801e5417e42deab3107f158437327d2c53dd9
Michael Budde <mbudde@gmail.com>
2014-10-26 12:58:20 +0100

    Update examples

---
+++ examples/generate_examples.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+output_dir=$(dirname $0)
+
+for f in ~/.smackage/lib/apltail/v1/tests/*.apl; do
+    output=$(basename $f)
+    output=$output_dir/${output%.apl}.tail
+    aplt -p_tail -noopt -o "$output" $f
+done
