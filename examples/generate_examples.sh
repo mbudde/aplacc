01a35f3d9915166581b0fddd19ffc4f9422f157c
Michael Budde <mbudde@gmail.com>
2014-10-28 20:22:05 +0100

    Add instance lists to example programs

--- examples/generate_examples.sh
+++ examples/generate_examples.sh
@@ -5,5 +5,5 @@ output_dir=$(dirname $0)
 for f in ~/.smackage/lib/apltail/v1/tests/*.apl; do
     output=$(basename $f)
     output=$output_dir/${output%.apl}.tail
-    aplt -p_tail -noopt -o "$output" $f
+    aplt -p_tail -p_types -noopt -o "$output" $f
 done
