1b5d59368315540fb6fdff34201e1386427c290e
Michael Budde <mbudde@gmail.com>
2014-10-31 10:29:03 +0100

    Include prelude.apl when generating examples

--- examples/generate_examples.sh
+++ examples/generate_examples.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+input_dir=~/.smackage/lib/apltail/v1/tests
 output_dir=$(dirname $0)
 
-for f in ~/.smackage/lib/apltail/v1/tests/*.apl; do
+for f in $input_dir/*.apl; do
     output=$(basename $f)
     output=$output_dir/${output%.apl}.tail
-    aplt -p_tail -p_types -noopt -o "$output" $f
+    aplt -p_tail -p_types -noopt -o "$output" "$input_dir/../prelude.apl" $f
 done
