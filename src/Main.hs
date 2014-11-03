8e8d989328bad476043ee340dca569e36ca50012
Michael Budde <mbudde@gmail.com>
2014-11-03 18:54:42 +0100

    Update usage string

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ main =
   do args <- getArgs
      case parseArgs args defaultOpts of
        ([f], opts) -> parseFile f >>= putStrLn . toHs opts . convertProgram
-       _           -> putStrLn "usage: aplacc <file>"
+       _           -> putStrLn "usage: aplacc [-c|--cuda] <file>"
   where parseArgs ("-c" : rest) opts     = parseArgs rest (opts { toCUDA = True })
         parseArgs ("--cuda" : rest) opts = parseArgs rest (opts { toCUDA = True })
         parseArgs (x : rest) opts        = let (as, opts') = parseArgs rest opts
