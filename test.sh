c09ef224f1cf3a938b91617a0fd809bb96d5b728
Michael Budde <mbudde@gmail.com>
2015-03-02 12:49:11 +0100

    Add option to move test files to test script

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ toplevel=$(dirname $0)
 verbose=
 write_output=
 files=()
+move=
 
 while [ $# != 0 ]; do
     case "$1" in
@@ -13,6 +14,8 @@ while [ $# != 0 ]; do
             write_output=1 ;;
         -a|--all)
             files=($toplevel/tests/working/*.tail $toplevel/tests/failing/*.tail) ;;
+        -m|--move)
+            move=1 ;;
         --)
             shift
             files+=("$@")
@@ -22,6 +25,14 @@ while [ $# != 0 ]; do
     esac
     shift
 done
+
+move_to() {
+    [ $move ] || return;
+    dest="$2/$(basename "$1")"
+    if [ ! -f "$dest" ]; then
+        mv "$1" "$dest"
+    fi
+}
 
 pushd $toplevel
 if ! cabal build; then
@@ -36,6 +47,10 @@ if [ $verbose ]; then
 fi
 
 for f in "${files[@]}"; do
+    if [ ! -r "$f" ]; then
+        echo -e "\033[31;1m>>> [$f] File not found\033[0m"
+        continue
+    fi
     if [ $write_output ]; then
         output="${f%.tail}.hs"
     fi
@@ -47,6 +62,7 @@ for f in "${files[@]}"; do
     hsoutput=$($toplevel/dist/build/aplacc/aplacc "$f")
     if [ $? != 0 ]; then
         echo -e "\033[31;1m<<< [$f] aplacc failed\033[0m"
+        move_to "$f" "$toplevel/tests/failing"
         continue
     fi
     if [ -n "$output" ]; then
@@ -56,7 +72,9 @@ for f in "${files[@]}"; do
     echo "$hsoutput" | runghc "-i$toplevel/src"
     if [ $? = 0 ]; then
         echo -e "\033[32m<<< [$f] Success\033[0m"
+        move_to "$f" "$toplevel/tests/working"
     else
         echo -e "\033[31;1m<<< [$f] ghc failed\033[0m"
+        move_to "$f" "$toplevel/tests/failing"
     fi
 done
