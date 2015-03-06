4989f6b02147dd76e850a4259d6c70cc7bca1780
Michael Budde <mbudde@gmail.com>
2015-03-06 11:20:55 +0100

    Changes to test generation script

--- tests/generate_examples.sh
+++ tests/generate_examples.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-input_dir=~/.smackage/lib/apltail/v1/tests
+if [ ! -x "$APLT" ]; then
+    echo "APLT env var not set"
+    exit 1
+fi
 output_dir=$(dirname $0)
+input_dir=$(dirname "$APLT")/tests
 
-for f in $input_dir/*.apl; do
+for f in ${@:-$input_dir/*.apl}; do
     output=$(basename $f)
     output=$output_dir/${output%.apl}.tail
-    aplt -p_tail -p_types -noopt -o "$output" "$input_dir/../prelude.apl" $f
+    echo "[32mprocessing ${f}...[0m"
+    $APLT -c -s_tail -p_tail -p_types -o "$output" "$input_dir/../lib/prelude.apl" $f
 done
