93cf58411e6f2df13842be840e66fecd4c3dcfea
Michael Budde <mbudde@gmail.com>
2015-03-17 20:37:01 +0100

    Output info messages on stderr

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,26 @@ module Main where
 import Data.Maybe (fromMaybe, maybeToList)
 import Data.List (intercalate)
 import Control.Monad (when)
-import System.IO (IOMode(ReadMode), withFile, stdin, Handle, hGetContents)
+import System.IO (IOMode(ReadMode), withFile, stdin, stderr, Handle, hPutStr, hPutStrLn, hGetContents)
 import System.Environment
 import System.Process (readProcessWithExitCode)
-import System.Exit (ExitCode(..))
+import System.Exit (exitFailure, ExitCode(..))
 
 import APLAcc.TAIL.Parser (parseString)
 import APLAcc.Conversion (convertProgram)
 import APLAcc.SimpleAcc.ToHaskell (toHs, OutputOpts(..), defaultOpts)
 
+putErr = hPutStr stderr
+putErrLn = hPutStrLn stderr
+
+usage :: IO ()
+usage =
+  do putErrLn "Usage: aplacc [options] <file>\n\n\
+              \  -v, --verbose          verbose output\n\
+              \  -t, --tail             input file is a TAIL program\n\
+              \  -c, --cuda             use CUDA backend\n\
+              \  --run                  run Accelerate program"
+     exitFailure
 
 main :: IO ()
 main =
@@ -19,7 +30,7 @@ main =
      case parseArgs args defaultOpts of
        (["-"], opts) -> compileFile "stdin" opts stdin
        ([f],   opts) -> withFile f ReadMode (compileFile f opts)
-       _             -> putStrLn "usage: aplacc [-c|--cuda] <file>"
+       _             -> usage
   where parseArgs ("-c"        : rest) opts = parseArgs rest (opts { toCUDA = True })
         parseArgs ("--cuda"    : rest) opts = parseArgs rest (opts { toCUDA = True })
         parseArgs ("-t"        : rest) opts = parseArgs rest (opts { tailInput = True })
@@ -38,7 +49,7 @@ main =
                            else compileApl opts h
              tailProg <- parseString tailText file
              let hsText = toHs opts $ convertProgram tailProg
-             when (verbose opts) $ putStrLn "\ESC[33m[ Accelerate output ]\ESC[0m"
+             when (verbose opts) $ putErrLn "\ESC[33m[ Accelerate output ]\ESC[0m"
              when (verbose opts || not (runProgram opts)) $ putStrLn hsText
              when (runProgram opts) $ runGhc opts hsText
 
@@ -49,27 +60,31 @@ compileApl opts handle =
      aplt <- lookupEnv "APLT" >>= return . fromMaybe "aplt"
      let args = apltArgs ++ prelude ++ ["-"]
      when (verbose opts) $
-       putStrLn $ "\ESC[33m[ Running aplt ] " ++ intercalate " " (aplt : args) ++ "\ESC[0m"
+       putErrLn $ "\ESC[33m[ Running aplt ] " ++ intercalate " " (aplt : args) ++ "\ESC[0m"
      (exitcode, stdout, _) <- readProcessWithExitCode aplt args input
      when (verbose opts) $
-       putStrLn "\ESC[33m[ APLT output ]\ESC[0m" >>
-       putStrLn stdout
+       putErrLn "\ESC[33m[ APLT output ]\ESC[0m" >>
+       putErrLn stdout
      case exitcode of
        ExitSuccess -> return stdout
-       ExitFailure n -> error $ "aplt failed with code " ++ show n
+       ExitFailure n ->
+         do putErr stdout
+            putErrLn $ "aplacc: aplt failed with code " ++ show n
+            exitFailure
   where apltArgs = ["-c", "-s_tail", "-p_tail", "-p_types", "-silent"]
 
 runGhc :: OutputOpts -> String -> IO ()
 runGhc opts program =
   do args <- getArgs
      when (verbose opts) $
-       putStrLn $ "\ESC[33m[ Running with GHC ] " ++ intercalate " " ("runghc" : args) ++ "\ESC[0m"
+       putErrLn $ "\ESC[33m[ Running with GHC ] " ++ intercalate " " ("runghc" : args) ++ "\ESC[0m"
      (exitcode, stdout, stderr) <- readProcessWithExitCode "runghc" args program
      case exitcode of
-       ExitSuccess -> putStr stdout
+       ExitSuccess -> putErr stdout
        ExitFailure n ->
-         do putStr stderr
-            error $ "ghc failed with code " ++ show n
+         do putErr stderr
+            putErrLn $ "aplacc: ghc failed with code " ++ show n
+            exitFailure
   where getArgs =
           do home <- lookupEnv "APLACC_HOME" >>=
                        return . fromMaybe (error "APLACC_HOME env var not set")
